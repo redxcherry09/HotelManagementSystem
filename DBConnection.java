a66722676b82b85f09fedc764baf028767829296
ruby0909 <rs1929469@gmail.com>
2026-04-09 13:56:20 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,7 +5,7 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/hotel_advanced",
+                "jdbc:mysql://localhost:3306/hotel_management",
                 "root",
                 "root"
             );
